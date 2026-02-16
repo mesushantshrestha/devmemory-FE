3d6fff074fbfc353e25942aaef74e6d5f0958479
KILLERPLAY\kille <mesushantshrestha@gmail.com>
2026-02-16 17:32:56 -0500

    item details page edit options updated

--- src/app/features/capture/pages/item-detail-page/item-detail-page.ts
+++ src/app/features/capture/pages/item-detail-page/item-detail-page.ts
@@ -1,28 +1,120 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { catchError, map, Observable, of, switchMap } from 'rxjs';
 import { CaptureItem } from '../../../../core/models/capture-item';
 import { CommonModule } from '@angular/common';
 import { CaptureApiService } from '../../../../core/services/capture-api-service';
+import { CaptureService } from '../../../../core/services/capture';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-item-detail-page',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './item-detail-page.html',
   styleUrl: './item-detail-page.scss',
 })
 export class ItemDetailPage implements OnInit {
   item$!: Observable<CaptureItem | undefined>;
+  isEditing = false;
+  editText = '';
+  editDone = false;
+  confirmDelete = false;
+  constructor(private route: ActivatedRoute,
+    private api: CaptureApiService,
+    private router: Router,
+    private captureService: CaptureService) { }
 
-  constructor(private route: ActivatedRoute, private api: CaptureApiService) { }
+  private loadItem(id: string): Observable<CaptureItem | undefined> {
+    return this.api.getById(id).pipe(
+      catchError(() => of(undefined))
+    );
+  }
+
   ngOnInit(): void {
     this.item$ = this.route.paramMap.pipe(
       map((params) => params.get('id')!),
-      switchMap((id) => {
-        return this.api.getById(id).pipe(
-          catchError(() => of(undefined))
-        );
-      })
+      switchMap((id) => this.loadItem(id))
     );
   }
+
+  startEdit(item: CaptureItem) {
+    this.isEditing = true;
+    this.editText = item.text ?? '';
+    this.editDone = !!item.done;
+  }
+
+  cancelEdit() {
+    this.isEditing = false;
+  }
+
+  saveEdit(item: CaptureItem) {
+    const patch: any = { text: this.editText };
+
+    // only allow done toggle for TASK
+    if (String(item.type).toLowerCase() === 'task') {
+      patch.done = this.editDone;
+    }
+
+    const optimisticItem: CaptureItem = {
+      ...item,
+      text: this.editText,
+      done: String(item.type).toLowerCase() === 'task' ? this.editDone : item.done,
+    };
+
+    this.item$ = of(optimisticItem);
+    this.isEditing = false;
+
+    this.captureService.update(item.id, patch).subscribe({
+      next: () => {
+        this.item$ = this.loadItem(item.id);
+      },
+      error: () => {
+        this.item$ = this.loadItem(item.id);
+      }
+    });
+  }
+
+  toggleTaskDone(item: CaptureItem) {
+    if (String(item.type).toLowerCase() !== 'task' || this.confirmDelete) {
+      return;
+    }
+
+    const nextDone = !item.done;
+    const optimisticItem: CaptureItem = {
+      ...item,
+      done: nextDone,
+    };
+
+    this.item$ = of(optimisticItem);
+    if (this.isEditing) {
+      this.editDone = nextDone;
+    }
+
+    this.captureService.update(item.id, { done: nextDone }).subscribe({
+      next: () => {
+        this.item$ = this.loadItem(item.id);
+      },
+      error: () => {
+        this.item$ = this.loadItem(item.id);
+      }
+    });
+  }
+
+  requestDelete() {
+    this.confirmDelete = true;
+  }
+
+  cancelDelete() {
+    this.confirmDelete = false;
+  }
+
+  doDelete(item: CaptureItem) {
+    this.captureService.remove(item.id).subscribe({
+      next: () => {
+        this.confirmDelete = false;
+        this.router.navigate(['/saved']);
+      }
+    });
+  }
+
 }
