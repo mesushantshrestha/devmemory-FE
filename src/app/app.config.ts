421f843eaeeea64edb8178cfce9c11e5881c8865
sushant <mesushantshrestha@gmail.com>
2026-02-18 21:17:06 -0500

    pwa introduced

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,6 +7,7 @@ import { CAPTURE_REPOSITORY } from './core/repositories/capture.repository';
 import { LocalCaptureRepository } from './core/repositories/local-capture.repository';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiErrorInterceptor } from './core/interceptors/api-error.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     // Repository binding (swap this later to ApiCaptureRepository)
-    { provide: CAPTURE_REPOSITORY, useClass: LocalCaptureRepository },
+    { provide: CAPTURE_REPOSITORY, useClass: LocalCaptureRepository }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
